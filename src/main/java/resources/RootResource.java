f2b3c96158cb2d098e9e5801017b9061974d3cc7
Mohammad Ghalandari <m.ghalandari@ostfalia.de>
2023-10-11 19:11:27 +0200

    in pom.xml die <finalName>ROOT</finalName> auf ROOT gesetzt, um beim Starten von Service direkt auf localhost:8081/ geht und nicht mit starter/api. den Path von JaxRsActivator.java auch auf Path('/') anstatt Path('api') gesetzt.

--- src/main/java/resources/RootResource.java
+++ src/main/java/resources/RootResource.java
@@ -2,13 +2,17 @@ package resources;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Application;
 import jakarta.ws.rs.core.Response;
 
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
 @Path("/")
-public class RootResource {
+public class RootResource{
     @GET
     public Response getRoot() {
         // Implementierung
-        return Response.ok("Root of DVD-Rental Film").build();
+        return Response.ok("Root of DVD-Rental Film. Returns array of primary resources").build();
     }
 }
