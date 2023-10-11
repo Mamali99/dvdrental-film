2553325e3f9d4319bbb11ad8629b112663a5a97f
Mohammad Ghalandari <m.ghalandari@ostfalia.de>
2023-10-11 19:40:20 +0200

    Der RootResource gibt actors, films und languages paths als json file zurück.

--- src/main/java/resources/RootResource.java
+++ src/main/java/resources/RootResource.java
@@ -2,17 +2,32 @@ package resources;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.core.Application;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriInfo;
 
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
+import java.util.HashMap;
+import java.util.Map;
 
 @Path("/")
-public class RootResource{
+public class RootResource {
+
+    @Context
+    private UriInfo uriInfo;
+
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
     public Response getRoot() {
-        // Implementierung
-        return Response.ok("Root of DVD-Rental Film. Returns array of primary resources").build();
+        String baseUri = uriInfo.getBaseUri().toString();
+
+        Map<String, String> paths = new HashMap<>();
+        paths.put("actors", baseUri + "actors");
+        paths.put("films", baseUri + "films");
+        paths.put("languages", baseUri + "languages");
+
+
+        return Response.ok(paths).build();
     }
 }
