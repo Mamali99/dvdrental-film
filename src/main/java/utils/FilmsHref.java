ee7a39136c415a6e359709e073a416a0a8fd043b
Mohammad Ghalandari <m.ghalandari@ostfalia.de>
2023-11-16 20:02:00 +0100

    in FilmHref habe ich @JsonCreator weg gemacht. Nachher muss kontrolliert werden, ob zum Fehler führt.

--- src/main/java/utils/FilmsHref.java
+++ src/main/java/utils/FilmsHref.java
@@ -6,8 +6,15 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class FilmsHref {
     private String href;
 
+    /*
     @JsonCreator
     public FilmsHref(@JsonProperty("href") String href) {
+        this.href = href;
+    }
+
+     */
+
+    public FilmsHref(String href) {
         this.href = href;
     }
 
