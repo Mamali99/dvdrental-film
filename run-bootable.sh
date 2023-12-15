8e472a0d4ba222d5b67b2c5280579c865ac8fdd9
Mohammad Ghalandari <m.ghalandari@ostfalia.de>
2023-12-15 10:03:41 +0100

    run-bootable.sh angepasst

--- run-bootable.sh
+++ run-bootable.sh
@@ -1,8 +1,8 @@
 export postgresql_database=dvdrentalfilm
 export postgresql_user=postgres
 export postgresql_password=trust
-export postgresql_datasource=PostgresDS
-export postgresql_service_port=54321
+#export postgresql_datasource=PostgresDS
+#export postgresql_service_port=54321
 
 
 java -Djboss.http.port=8081 -jar target/dvdrental-film-bootable.jar
