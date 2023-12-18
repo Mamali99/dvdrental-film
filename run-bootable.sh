1a7c1fcdc0a561c5cbc2b6f1ae5735c58329e179
Mohammad Ghalandari <m.ghalandari@ostfalia.de>
2023-12-18 16:26:44 +0100

    http_port angepasst

--- run-bootable.sh
+++ run-bootable.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 export POSTGRES_HOST=localhost
-export POSTGRES_PORT=54321 # muss alle 5432 sein
+export POSTGRES_PORT=5432
 export POSTGRES_DB=dvdrentalfilm
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=trust
 
-# Setze den Port, auf dem der Service laufen soll
+
 export HTTP_PORT=8081
 
+mvn clean package
+
+if [ -f "target/dvdrental-film-bootable.jar" ]; then
 java -Djboss.http.port=${HTTP_PORT} \
      -Denv.POSTGRES_HOST=${POSTGRES_HOST} \
      -Denv.POSTGRES_PORT=${POSTGRES_PORT} \
@@ -16,3 +19,7 @@ java -Djboss.http.port=${HTTP_PORT} \
      -Denv.POSTGRES_USER=${POSTGRES_USER} \
      -Denv.POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
      -jar target/dvdrental-film-bootable.jar
+else
+    echo "Fehler beim Erstellen der JAR-Datei"
+    exit 1
+fi
